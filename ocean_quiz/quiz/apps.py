6f1f9a5544a4c7616031bd48b1f616e19a3a9a93
Atisemma <atisemma26@gmail.com>
2025-01-28 01:19:02 +0300

    Application configuration

--- ocean_quiz/quiz/apps.py
+++ ocean_quiz/quiz/apps.py
@@ -2,9 +2,5 @@ from django.apps import AppConfig
 
 
 class QuizConfig(AppConfig):
-    """
-   Configuration class for the Quiz application.
-   Specifies the database field type and application name.
-   """
    default_auto_field = 'django.db.models.BigAutoField'
    name = 'quiz'
