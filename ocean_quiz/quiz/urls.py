08b6b129e926ce956389379a9abef2b68bc20c90
Atisemma <atisemma26@gmail.com>
2025-01-28 00:09:06 +0300

    URL routing and mapping URLs to specific views

--- ocean_quiz/quiz/urls.py
+++ ocean_quiz/quiz/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import get_quiz, index
 
 urlpatterns = [
+    # Home page route
     path('', index, name='index'),
+
+    #API endpoint to fetch the quiz questions
     path('api/get-quiz/', get_quiz, name='get_quiz'),
 ]
